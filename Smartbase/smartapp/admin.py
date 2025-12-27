2efac992b293274687bf561adc6484a870fd8091
Ronit Shinde <shinderonit4@gmail.com>
2025-12-27 23:24:34 +0530

    yy

--- Smartbase/smartapp/admin.py
+++ Smartbase/smartapp/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
 
 # Register your models here.
+from django.contrib import admin
+from .models import Application
+
+@admin.register(Application)
+class ApplicationAdmin(admin.ModelAdmin):
+    list_display = (
+        'application_id',
+        'full_name',
+        'email',
+        'entrance_exam',
+        'score',
+        'institution',
+        'branch',
+        'status',
+        'created_at',
+    )
+    search_fields = ('application_id', 'full_name', 'email')
